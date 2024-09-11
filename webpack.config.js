e1dd34adc998b8ab3f77b9643d28e4f9873aafd6
Carlos López-Ochoa Aledo <carlos.lopezochoa@inercya.com>
2024-09-11 08:36:36 +0200

    exclude dev and focus on prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const isProd = process.argv.includes("production");
-
 /**
  * @type {import('webpack').Configuration}
  */
@@ -10,8 +8,7 @@ module.exports = [
   {
     entry: {
       "index": "./src/index.ts",
-      "quill.imageCompressor": "./src/quill.imageCompressor.ts",
-      demo: "./src/demo.js"
+      "quill.imageCompressor": "./src/quill.imageCompressor.ts"
     },
     output: {
       filename: (pathData) => {
@@ -21,21 +18,9 @@ module.exports = [
       libraryTarget: "umd",
       publicPath: "/dist/"
     },
-    devServer: {
-      port: 8001,
-      hot: true,
-      static: {
-        directory: path.join(__dirname, 'src'),
-        serveIndex: true,
-      },
-      devMiddleware: {
-        writeToDisk: true,
-      },
-    },
     externals: {
       quill: "Quill"
     },
-    devtool: isProd ? undefined : "inline-source-map",
     resolve: {
       extensions: [".ts", ".js"],
     },
